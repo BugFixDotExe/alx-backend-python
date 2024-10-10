db7988d4c563e24da4c5b1caeebf5b76c8f2558c
BugFixDotExe <bugfix.exe@gmail.com>
2024-10-10 15:51:47 +0100

    solution to problem 7 testing time

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
-from typing import Union
-''' '''
-def to_kv(k: str , v: Union([int| float])) -> tuple:
+from typing import Union, Tuple
+
+''' a type-annotated function to_kv that takes a string k and
+    an int OR float v as arguments and returns a tuple'''
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''A function that taken in str and a Union return a Tuple'''
+    '''
+        Args:
+            k: A string argunment
+            v: A Union Data Struct accepting types int || float
+        Return:
+            A tuple having types str && float
+    '''
+    return (k, v**2)
